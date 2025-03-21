044548ee34a7cee9177b29acca4cc1f02b1c41aa
Tono <limhao2705@gmail.com>
2025-03-21 16:58:16 +0700

    KDSP-021: Centralize icon usage for reusability.

---
+++ frontend/src/components/icons.tsx
@@ -0,0 +1,49 @@
+import {
+  Loader2,
+  LucideProps,
+  Moon,
+  SunMedium,
+  Laptop,
+  // Google,
+  Github,
+  Mail,
+  User,
+  Settings,
+  LogOut,
+  ChevronLeft,
+  ChevronRight,
+  Plus,
+  Trash,
+  Upload,
+  FileText,
+  // AudioLines,
+  Check,
+  X,
+} from "lucide-react";
+
+import { FcGoogle } from "react-icons/fc"
+
+export type Icon = LucideProps;
+
+export const Icons = {
+  sun: SunMedium,
+  moon: Moon,
+  laptop: Laptop,
+  // google: Google,
+  github: Github,
+  mail: Mail,
+  user: User,
+  settings: Settings,
+  logout: LogOut,
+  chevronLeft: ChevronLeft,
+  chevronRight: ChevronRight,
+  plus: Plus,
+  trash: Trash,
+  upload: Upload,
+  fileText: FileText,
+  // audioLines: AudioLines,
+  check: Check,
+  x: X,
+  spinner: Loader2,
+  google: FcGoogle,
+}; 
